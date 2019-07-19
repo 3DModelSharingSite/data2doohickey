d855ee6ed4fa54fa814080d205a7260b4cb85b22
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-07-19 09:05:13 -0500

    Reverted filestack urlscreenshot test

--- src/main/java/com/codeup/d2d/controllers/DoohickeyController.java
+++ src/main/java/com/codeup/d2d/controllers/DoohickeyController.java
@@ -115,6 +115,9 @@ public class DoohickeyController {
 
     @GetMapping("/doohickeys/create")
     public String showCreateDoohickeyForm(Model model){
+        if(authSvc.getCurUser() == null){
+            return "redirect:/login";
+        }
         model.addAttribute("doohickey", new Doohickey());
         model.addAttribute("action", "/doohickeys/create");
         model.addAttribute("title", "Create a Doohickey");
