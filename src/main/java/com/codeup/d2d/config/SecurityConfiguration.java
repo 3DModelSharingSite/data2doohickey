d2f15fda17d65bcd88c75bce706f301b0fdfb98e
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-07-24 09:35:00 -0500

    Updated Spring Security configuration to match specific pages

--- src/main/java/com/codeup/d2d/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/d2d/config/SecurityConfiguration.java
@@ -46,15 +46,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/posts") // anyone can see the home and the ads pages
+                .antMatchers("/", "/doohickeys") // anyone can see the home and the ads pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
                 .authorizeRequests()
                 .antMatchers(
-                        "/posts/create", // only authenticated users can create ads
-                        "/posts/{id}/edit", // only authenticated users can edit ads
-                        "/posts/{id}/delete" // only authenticated users can edit ads
+                        "/doohickeys/create", // only authenticated users can create ads
+                        "/doohickeys/{id}/edit", // only authenticated users can edit ads
+                        "/doohickeys/{id}/delete" // only authenticated users can edit ads
                 )
                 .authenticated()
         ;
