5c2a181d56977be16023e153ae61cc0fc1ab156e
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-07-24 21:19:16 -0500

    Changed order of doohickey lists Made profile buttons direct to actual pages

--- src/main/java/com/codeup/d2d/repos/DoohickeyRepository.java
+++ src/main/java/com/codeup/d2d/repos/DoohickeyRepository.java
@@ -1,6 +1,7 @@
 package com.codeup.d2d.repos;
 
 import com.codeup.d2d.models.Doohickey;
+import com.codeup.d2d.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
@@ -11,8 +12,7 @@ import java.util.List;
 
 @Repository
 public interface DoohickeyRepository extends CrudRepository<Doohickey, Long> {
-    List<Doohickey> findByTitle(String findTitle);
-    List<Doohickey> findByDescription(String findDescription);
-    Page<Doohickey> findByTitleIsLikeOrDescriptionIsLike(String term,String term2,Pageable pageable);
-    Page<Doohickey> findAll(Pageable pageable);
+    Page<Doohickey> findByTitleIsLikeOrDescriptionIsLikeOrderByIdDesc(String term,String term2,Pageable pageable);
+    Page<Doohickey> findByAuthorOrderByIdDesc(User user,Pageable pageable);
+    Page<Doohickey> findByUsersFavoritedOrderByIdDesc(User user, Pageable pageable);
 }
